aeea02a5bd7c8a499d441aed4aebba9b2cd052f6
mahmoud-300 <80002645+eng-mahmoud-hamada@users.noreply.github.com>
2024-02-26 20:49:11 +0300

    modified:   poll_app/script.js

---
+++ poll_app/script.js
@@ -0,0 +1,20 @@
+        
+  const options=[
+  {id:"option1",text:"option1",votes:0},
+  {id:"option1",text:"option1",votes:0},
+  {id:"option1",text:"option1",votes:0},
+  {id:"option1",text:"option1",votes:0},
+  ];    
+              
+function submitVote(){
+ const selectedOption=document.querySelector('input[name="poll"]:checked');
+
+  if(!selectedOption){
+alert("please select any option");
+   return;
+
+  }
+
+
+
+}
