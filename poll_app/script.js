4ae89b248133ff4e4b515ba3ac91b173c83f36cf
mahmoud-300 <80002645+eng-mahmoud-hamada@users.noreply.github.com>
2024-02-26 21:48:29 +0300

    modified:   poll_app/script.js

--- poll_app/script.js
+++ poll_app/script.js
@@ -1,20 +1,23 @@
         
   const options=[
   {id:"option1",text:"option1",votes:0},
-  {id:"option1",text:"option1",votes:0},
-  {id:"option1",text:"option1",votes:0},
-  {id:"option1",text:"option1",votes:0},
+  {id:"option2",text:"option2",votes:0},
+  {id:"option3",text:"option3",votes:0},
+  {id:"option4",text:"option4",votes:0},
   ];    
               
 function submitVote(){
  const selectedOption=document.querySelector('input[name="poll"]:checked');
-
+ //console.log(selectedOption.value);
   if(!selectedOption){
 alert("please select any option");
    return;
 
   }
+  const optionId = selectedOption.value;
+  const selectedElement=options.find((option)=>option.id===optionId);
+ if(selectedElement){
 
-
+ }
 
 }
