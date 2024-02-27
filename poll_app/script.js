23d70f21714eac780a351b20dd33bbb9632cb16a
mahmoud-300 <80002645+eng-mahmoud-hamada@users.noreply.github.com>
2024-02-27 22:16:01 +0300

    modified:   poll_app/script.js 	modified:   poll_app/style.css

--- poll_app/script.js
+++ poll_app/script.js
@@ -17,7 +17,38 @@ alert("please select any option");
   const optionId = selectedOption.value;
   const selectedElement=options.find((option)=>option.id===optionId);
  if(selectedElement){
-
+ selectedElement.votes++;
+ displayResult();
  }
 
 }
+
+function displayResult(){
+    const result=document.getElementById('result');
+    result.innerHTML="";
+   options.forEach((option)=>{
+    const percentage=((option.votes/getTotalVotes())*100).toFixed(2) || 0;
+    const barWidth = percentage > 0 ? percentage+"%":"0%";
+    const optionResult = document.createElement("div");
+    optionResult.className ="option-result";
+    optionResult.innerHTML =`
+    <span class="option-text">${option.text}</span>
+    <div class="bar-container">
+    <div class="bar" style="width:${barWidth};"></div>
+    </div>
+    <span class="percentage">${percentage}</span>
+
+    `;
+    result.appendChild(optionResult);
+   });
+
+
+
+
+}
+function getTotalVotes(){
+return options.reduce((total,option)=>total +option.votes,0);
+
+}
+
+displayResult();
